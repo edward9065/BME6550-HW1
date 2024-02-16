2c96900d672e63cba88cd0e695d40e26b4208035
Edward Chen <edward9065@gmail.com>
2024-02-16 00:32:18 -0500

    improve plots - show ground truth plots and add rand index

--- hw1.py
+++ hw1.py
@@ -36,17 +36,41 @@ pca = PCA(n_components=2)
 cho_pca = pca.fit_transform(cho_data[:,2:])
 iyer_pca = pca.fit_transform(iyer_data[:,2:])
 
-fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12, 12))
+fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(18, 12))
 
-axs[0,0].scatter(cho_pca[:,0], cho_pca[:,1], c=cho_kmeans, cmap="viridis")
-axs[0,0].set_title("K-Means on cho.txt")
-axs[0,1].scatter(cho_pca[:,0], cho_pca[:,1], c=cho_gm, cmap="viridis")
-axs[0,0].set_title("Gaussian Mixtures on cho.txt")
-axs[1,0].scatter(iyer_pca[:,0], iyer_pca[:,1], c=iyer_kmeans, cmap="viridis")
-axs[0,0].set_title("K-Means on iyer.txt")
-axs[1,1].scatter(iyer_pca[:,0], iyer_pca[:,1], c=iyer_gm, cmap="viridis")
-axs[0,0].set_title("Gaussian Mixtures on iyer.txt")
+axs[0,0].scatter(cho_pca[:,0], cho_pca[:,1], c=cho_data[:,1], cmap="viridis")
+axs[0,0].set_title("Ground Truth of cho.txt")
+
+axs[0,1].scatter(cho_pca[:,0], cho_pca[:,1], c=cho_kmeans, cmap="viridis")
+axs[0,1].set_title("K-Means on cho.txt")
+axs[0,1].annotate(f"Rand Index {round(cho_kmeans_rand_score, 4)}", (4,4))
+
+axs[0,2].scatter(cho_pca[:,0], cho_pca[:,1], c=cho_gm, cmap="viridis")
+axs[0,2].set_title("Gaussian Mixtures on cho.txt")
+axs[0,2].annotate(f"Rand Index {round(cho_gm_rand_score, 4)}", (4,4))
+
+
+
+axs[1,0].scatter(iyer_pca[:,0], iyer_pca[:,1], c=iyer_data[:,1], cmap="viridis")
+axs[1,0].set_title("Ground Truth of iyer.txt")
+# remove the one point that squishes the graph by alot
+axs[1,0].set_xlim(-10,40)
+axs[1,0].set_ylim(-20,5)
+
+axs[1,1].scatter(iyer_pca[:,0], iyer_pca[:,1], c=iyer_kmeans, cmap="viridis")
+axs[1,1].set_title("K-Means on iyer.txt")
+axs[1,1].set_xlim(-10,40)
+axs[1,1].set_ylim(-20,5)
+axs[1,1].annotate(f"Rand Index {round(iyer_kmeans_rand_score, 4)}", (22,4))
+
+
+axs[1,2].scatter(iyer_pca[:,0], iyer_pca[:,1], c=iyer_gm, cmap="viridis")
+axs[1,2].set_title("Gaussian Mixtures on iyer.txt")
+axs[1,2].set_xlim(-10,40)
+axs[1,2].set_ylim(-20,5)
+axs[1,2].annotate(f"Rand Index {round(iyer_gm_rand_score, 4)}", (22,4))
 
 
 plt.show()
+plt.savefig("plot.png")
 # %%
